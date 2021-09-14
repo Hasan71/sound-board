1203b48d89ec4769f83cfea86b78af235820b965
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-09-14 23:51:38 +0600

    added css

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button');
-    btn.classList.add('add');
+    btn.classList.add('btn');
 
     btn.innerHTML = sound;
 

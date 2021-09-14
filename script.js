c2bdd94c4b63da3f7601fda54333c5e39a492276
Mahmudul Hasan <mahmud.cse7@gmail.com>
2021-09-15 00:01:31 +0600

    fixed playing sound over one another

--- script.js
+++ script.js
@@ -7,9 +7,18 @@ sounds.forEach(sound => {
     btn.innerHTML = sound;
 
     btn.addEventListener ('click', () => {
+        stopSound();
         document.getElementById(sound).play();
     })
 
     document.getElementById('buttons').appendChild(btn);
-
 })
+
+//to avoid playing over one another
+function stopSound () {
+    sounds.forEach(sound => {
+        const music = document.getElementById(sound);
+        music.pause();
+        music.currentTime = 0;
+    })
+}
